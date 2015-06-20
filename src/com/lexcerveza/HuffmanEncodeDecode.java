1190fc8bf5afb09bc68214ab85aa8abebbca4dbd
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-06-20 21:15:23 +0300

    Test commit for backup

--- src/com/lexcerveza/HuffmanEncodeDecode.java
+++ src/com/lexcerveza/HuffmanEncodeDecode.java
@@ -1,9 +1,6 @@
 package com.lexcerveza;
 
-import com.sun.deploy.util.StringUtils;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Alex on 20.06.2015.
@@ -22,5 +19,26 @@ public class HuffmanEncodeDecode {
             int count = sourceString.length() - sourceString.replace(String.valueOf(targetChar), "").length();
             map.put(targetChar, count);
         }
+
+        // Secondly, build a tree of nodes and sort it
+        List<Node> nodes = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : map.entrySet()){
+            nodes.add(new Node(entry.getKey(), entry.getValue()));
+        }
+
+        Comparator<Node> nodeComparator = (node1, node2) -> node1.count - node2.count;
+        while(nodes.size() != 1) {
+            nodes.sort(nodeComparator);
+            Node left = nodes.get(0);
+            nodes.remove(left);
+            Node right = nodes.get(0);
+            nodes.remove(right);
+
+            Node parent = new Node(left, right);
+            nodes.add(parent);
+        }
+
+        // Debug thing.
+        int i = 1;
     }
 }
